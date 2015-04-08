d6c3f47c349b075286a27dc0f1fa7904042d9f6b
cogbee <cogbee@163.com>
2015-04-08 22:35:22 +0800

    Signed-off-by: cogbee <cogbee@163.com>

--- sms.py
+++ sms.py
@@ -9,13 +9,16 @@ time:2015-3-29
 import httplib2
 import urllib  
 import threadpool
+import thread
 
 class sendsms(object):
 	def __init__(self,phone,num):
 		self.phone = phone
 		self.url = 'http://open.epicc.com.cn/eplatform/loginRegister/register/saveYZM'
 		self.threadnum = 5
-		self.all = num
+		self.all = int(num)
+		self.count = 0
+		self.lock = thread.allocate_lock()
 
 	def sends(self):
 		h = httplib2.Http()
@@ -23,7 +26,11 @@ class sendsms(object):
 		headers = {'Content-type': 'application/x-www-form-urlencoded','Origin':'http://open.epicc.com.cn','Referer':'http://open.epicc.com.cn/eplatform/views/loginRegister/register.jsp','User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.94 Safari/537.36',
 		'Cookie':'epicc_tid=10.130.67.33.1427628209097650; s_nr=1427630306223-New; _gscu_321070471=27628260i49hyf19; _gscu_793357708=27630350n6xe0314; _gscs_793357708=27640823gjn62i14|pv:1; _gscbrs_793357708=1; EcutH7WVc8=MDAwM2IyYWQzNzQwMDAwMDAwMDIwZjBEBQcxNDI3NjUwMzMx; VwS0QvsNPc=MDAwM2IyYWQ0ZjQwMDAwMDAwM2IwHGpvO0MxNDI3NjUyMjgz; JSESSIONID=2fyQVYSPRLX5cRsD8ZfKCzTmD9phjh83XnJYhJJRMnnH2GYyR1Tl!-731644981'}  
 		res,con = h.request(self.url,'POST',headers = headers,body=urllib.urlencode(body))
-		print con
+		if con.find('success') != -1:
+			self.lock.acquire()
+			self.count = self.count + 1
+			print 'this is the %s message' % self.count
+			self.lock.release()
 
 	def gothread(self):
 		#建立进程池
@@ -35,12 +42,13 @@ class sendsms(object):
 			pool.add_task(self.sends)
 		#join and destroy all threads
 		pool.destroy()
+		return self.count
 
 if __name__=='__main__':
 	phone = raw_input('please input a phone:\n')
 	num = raw_input('please input the number of sms you want to send\n')
 	test = sendsms(phone,num)
-	test.gothread()
-	raw_input('OK\n')
+	count = test.gothread()
+	raw_input('OK\n we have send %s messages successfully\n' % count)
 
 
